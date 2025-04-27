51732bcd5d9b0fc198e50e70ff08d8552ba6ca78
Alex <alejandromirandaco1801@gmail.com>
2025-04-27 19:46:33 +0200

    feat: Segundo enemigo que se gira al llegar al final de su recorrido

---
+++ Assets/Scripts/Enemies/Skeleton.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class Skeleton : CharacterBody2D
+{
+
+    private Vector2 velocity;
+    [Export] public AnimatedSprite2D sprite;
+    
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+
+        velocity.Y += GetGravity().Y * (float)delta;
+
+
+        Velocity = velocity;
+        MoveAndSlide();
+    }
+}
--- Scenes/Niveles/PathFollow2d.cs
+++ Scenes/Niveles/PathFollow2d.cs
@@ -4,16 +4,31 @@ using System;
 public partial class PathFollow2d : PathFollow2D
 {
 
-    public float speed = 0.1f;
+    [Export] public float speed = 0.1f;
     public bool isMoonWalking = false;
+    public bool isSkeleton = false;
 
+    public Skeleton skeleton;
 
+    public override void _Ready()
+    {
+        base._Ready();
+        skeleton = GetChild(0) as Skeleton;
+
+        isSkeleton = skeleton != null;
+    }
 
     public override void _Process(double delta)
     {
+
         if (isMoonWalking)
         {
             ProgressRatio -= speed * (float)(delta);
+            if (isSkeleton)
+            {
+                skeleton.sprite.FlipH = true;
+            }
+
             if (ProgressRatio <= 0)
             {
                 isMoonWalking = false;
@@ -22,10 +37,16 @@ public partial class PathFollow2d : PathFollow2D
         else
         {
             ProgressRatio += speed * (float)(delta);
+
+            if (isSkeleton){
+                skeleton.sprite.FlipH = false;
+            }
+
             if (ProgressRatio >= 1)
             {
                 isMoonWalking = true;
             }
         }
+
     }
 }
