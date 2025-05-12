cd02357ebb07e92b0a210ac9c1609550f923adcc
Alex <alejandromirandaco1801@gmail.com>
2025-05-12 23:43:10 +0200

    feat: Limites de las camaras en cada escena

--- Scenes/Niveles/Area2d.cs
+++ Scenes/Niveles/Area2d.cs
@@ -8,18 +8,17 @@ public partial class Area2d : Area2D
         if (body is Player player)
         {
             Camera2D camera = body.GetNode<Camera2D>("Camera2D");
-
             CollisionShape2D shapeNode = GetNode<CollisionShape2D>("CollisionShape2D");
 
             if (shapeNode.Shape is RectangleShape2D rectShape)
             {
                 Vector2 globalCenter = GlobalPosition;
-                Vector2 extents = rectShape.Size;
+                Vector2 size = rectShape.Size;
 
-                int left = Mathf.RoundToInt(globalCenter.X - extents.X);
-                int right = Mathf.RoundToInt(globalCenter.X + extents.X);
-                int top = Mathf.RoundToInt(globalCenter.Y - extents.Y);
-                int bottom = Mathf.RoundToInt(globalCenter.Y + extents.Y);
+                int left = Mathf.RoundToInt(globalCenter.X - (size.X / 2));
+                int right = Mathf.RoundToInt(globalCenter.X + (size.X) / 2);
+                int top = Mathf.RoundToInt(globalCenter.Y - (size.Y / 2));
+                int bottom = Mathf.RoundToInt(globalCenter.Y + (size.Y) / 2);
 
                 camera.LimitLeft = left;
                 camera.LimitRight = right;
